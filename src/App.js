d52f05d3b886e3fc266cd0fbc93eb88b6da2c7f0
rosscarnegie <97925198+rosscarnegie@users.noreply.github.com>
2022-03-02 17:05:19 -0500

    added progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const startVinyls = [];
+  const [currentVinyls, setVinyls] =  useState(startVinyls);
+
+  const vinylList = currentVinyls.map(vinyl => 
+    <li key={vinyl._id}>
+      {vinyl.artist}: {vinyl.album}
+    </li>
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+    
+    fetch('http://localhost:3000/vinyls/seeAll', requestOptions)
+      .then(response => response.json())
+      .then(data => setVinyls(data.vinyls))
+  }
+
+    const handleInputChange = (event) => {
+      event.preventDefault();
+      const requestOptions = {
+        method: 'POST',
+        redirect: 'follow',
+        body: `{}`}
+      
+      fetch('http://localhost:3000/vinyls/', requestOptions)
+        .then(response => response.json())
+        .then(data => setVinyls(data.vinyls))
+
+    }
+    
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSubmit}>Submit</button>
+      <ul>{vinylList}</ul>
+      <form className='coordForm' onSubmit={handleSubmit}>
+        <input type='text'  name='artist' placeholder='artist' value={artist} onChange={handleInputChange}/>
+        <input type='text'  name='album' placeholder='album' value={album} onChange={handleInputChange}/>
+        <button>Find</button>
+      </form>
     </div>
   );
 }
